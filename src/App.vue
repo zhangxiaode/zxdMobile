91b08d327d9ab2110b09c198711501889aa701bb
zhangxiaode <2279323443@qq.com>
2018-09-25 17:37:26 +0800

    page init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <a href="javascript:;" class="fullIcon" @click="toggleFullScreen"></a>
     <Footer></Footer>
     <router-view></router-view>
   </div>
@@ -15,12 +16,29 @@ export default {
     }
   },
   computed: {
+    isFullscreen() {
+      return document.fullScreen||document.mozFullScreen||document.webkitIsFullScreen
+    }
   },
   created () {
   },
   mounted () {
   },
   methods: {
+    toggleFullScreen(e){
+      var el=document.documentElement;
+      var isFullscreen=document.fullScreen||document.mozFullScreen||document.webkitIsFullScreen;
+      if(!isFullscreen){//进入全屏,多重短路表达式
+        (el.requestFullscreen&&el.requestFullscreen())||
+        (el.mozRequestFullScreen&&el.mozRequestFullScreen())||
+        (el.webkitRequestFullscreen&&el.webkitRequestFullscreen())||(el.msRequestFullscreen&&el.msRequestFullscreen());
+
+      }else{	//退出全屏,三目运算符
+        document.exitFullscreen?document.exitFullscreen():
+        document.mozCancelFullScreen?document.mozCancelFullScreen():
+        document.webkitExitFullscreen?document.webkitExitFullscreen():'';
+      }
+    }
   }
 }
 </script>
@@ -29,5 +47,14 @@ export default {
 #app {
   position:relative;width:100%;height:100%;
   overflow-y: auto;overflow-x: hidden;
+  .fullIcon{
+    position:fixed;right:0.03rem;top:50%;width:1rem;height:1rem;
+    background:rgba(0,0,0,0.2);border-radius:0.2rem;
+    &:after{
+      border-radius:0.3rem;background:rgba(255,255,255,0.2);
+      display:block;width:0.6rem;height:0.6rem;
+      margin:0.2rem;content:'';
+    }
+  }
 }
 </style>
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="header">
-    <router-link v-for="(item,index) in navList" :key="index" @click="changeNav(index)" :to="item.path">{{item.value}}</router-link>
+  <div class="footer">
+    <router-link v-for="(item,index) in navList" :key="index" @click="changeNav(index)" :to="item.path">
+      <span><i class="icon iconfont" v-html="item.icon"></i>{{item.value}}</span>
+    </router-link>
   </div>
 </template>
 
@@ -16,14 +18,17 @@ export default {
       navList:[
         {
           path: "/homepage",
+          icon: "&#xe6cb;",
           value: "首页"
         },
         {
           path: "/travel",
+          icon: "&#xe67f;",
           value: "旅游"
         },
         {
           path: "/synopsis",
+          icon: "&#xe657;",
           value: "简介"
         }
       ]
@@ -39,7 +44,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-.header{
+.footer{
   position:fixed;bottom:0;left:0;z-index:99;width:100%;height:1.02rem;
   display:flex;justify-content: space-around;align-items: center;
   border-top:solid 0.02rem #F5F5F5;
@@ -49,15 +54,13 @@ export default {
     color:#5A5A5A;font-size:0.32rem;
     flex:1;text-align:center;
     cursor:pointer;
+    i{
+      margin-right:0.05rem;
+    }
     &.router-link-active{
-      color:#222222;font-weight:bold;
+      color:#fff;
       position:relative;
-      &:after{
-        position:absolute;left:50%;bottom:0;margin-left:-0.23rem;
-        border-radius:0.03rem;background:#FF8C53;
-        width:0.46rem;height:0.06rem;
-        content:'';
-      }
+      background:#68f;
     }
   }
 }
